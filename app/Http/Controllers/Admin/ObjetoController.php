c7761787a142adfed96ed87dfff3aff3167022ae
Edgard Lanza <129351520+edgard521@users.noreply.github.com>
2025-07-17 19:31:34 -0600

    correcion de objeto

--- app/Http/Controllers/Admin/ObjetoController.php
+++ app/Http/Controllers/Admin/ObjetoController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Objeto;
+use Illuminate\Support\Facades\Auth;
+
 
 class ObjetoController extends Controller
 {
