7438c3313e59554f8603c3af1b0539910ee2bc98
JoseTrejo01 <armandoarmandito79@gmail.com>
2025-09-11 22:02:00 -0600

    un error que habia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,1 @@
 import './bootstrap';
-import 'jquery';
-import 'bootstrap';
-import 'admin-lte';
-import 'overlayscrollbars';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'admin-lte/dist/css/adminlte.min.css';
-import 'overlayscrollbars/css/OverlayScrollbars.min.css';
